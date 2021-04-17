1775a1c5f155759b223be9c63151c358ec7e172d
Bhanu <6038051-Bhanu951@users.noreply.gitlab.com>
2021-04-17 20:26:23 +0530

    Initial Commit.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Bhanu Blog`,
+    descrption: `Bhanu Blog based on Tailwind`,
+    author: `@Bhanu`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-postcss`,
     `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
